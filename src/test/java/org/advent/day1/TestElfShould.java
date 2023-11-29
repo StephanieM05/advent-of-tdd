3b40296a5013752b1ea4b3e80e09e0781cd1d3ba
Stephanie Mansour <stephaniemansour@Stephanies-MacBook-Pro.local>
2023-11-29 12:43:08 -0500

    completed exercise-1

--- src/test/java/org/advent/day1/TestElfShould.java
+++ src/test/java/org/advent/day1/TestElfShould.java
@@ -16,21 +16,42 @@ public class TestElfShould {
 
     @Test
     void have_1000_total_calories_after_adding_1000() {
-       fail("Not yet implemented");
+        Elf elf = new Elf();
+        elf.addCalories(1000);
+        assertThat(elf.getTotalCalories(), equalTo(1000));
     }
 
     @Test
     void compare_return_positive_value_for_elf_with_more_total_calories() {
-        fail("Not yet implemented");
+        Elf elf1 = new Elf();
+        Elf elf2 = new Elf();
+        elf1.addCalories(2000);
+
+        int difference = elf1.compareTo(elf2);
+        assertThat(difference, greaterThan(0));
     }
 
     @Test
     void compare_zero_for_equal_elf() {
-        fail("Not yet implemented");
+        Elf elf1 = new Elf();
+        Elf elf2 = new Elf();
+
+        elf1.addCalories(2000);
+        elf2.addCalories(2000);
+        
+        int difference = elf1.compareTo(elf2);
+        assertThat(difference, equalTo(0));
     }
 
     @Test
     void compare_return_negative_value_for_elf_with_less_total_calories() {
-        fail("Not yet implemented");
+        Elf elf1 = new Elf();
+        Elf elf2 = new Elf();
+
+        elf1.addCalories(1000);
+        elf2.addCalories(2000);
+        
+        int difference = elf1.compareTo(elf2);
+        assertThat(difference, lessThan(0));
     }
 }
